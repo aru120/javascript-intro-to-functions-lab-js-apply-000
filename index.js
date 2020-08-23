e5da149abaaa3f72c51a17fc6fb0b634612bb52d
Kevin Huang <khuang159@gmail.com>
2020-08-23 22:38:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,16 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string === string.toUpperCase())
   {
+    return "YES INDEED!"
+  }
+  else if (string === string.toLowerCase())
+  {
+    return "I can't hear you!"
+  }
+  else if (string === "I love you")
+  {
     
+    return "I love you, too."
   }
   
   return string.toLowerCase()
